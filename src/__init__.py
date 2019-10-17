9e8710131be19280b9e79bb5826d07e76ae347ae
Jiglionero <smouk.chayz@gmail.com>
2019-10-17 18:49:20 +0300

    min workable

---
+++ src/__init__.py
@@ -0,0 +1,31 @@
+#!flask/bin/python
+from flask import Flask
+from flask import request
+from src.data import open_weather_api as Api
+
+app = Flask(__name__)
+
+
+@app.route('/current')
+def current():
+    return Api.get_current_weather(define_args(request.args))
+
+
+@app.route('/forecast')
+def forecast():
+    return Api.get_forecast_5d3h_weather(define_args(request.args))
+
+
+def define_args(args):
+    city = args.get('city')
+    lat = args.get('lat')
+    lon = args.get('lon')
+    if city:
+        return {'q': city}
+    else:
+        return {'lat': lat,
+                'lon': lon}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
